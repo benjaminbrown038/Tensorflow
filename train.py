15e69896bbf63803ca69cc9e8644e5a1ff2f3530
Benjamin Brown <benjaminbrown038@gmail.com>
2023-04-30 16:14:02 -0600

    working

---
+++ train.py
@@ -0,0 +1,7 @@
+import tensorflow
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.models import Conv2D,MaxPool2D,Flatten,
+
+
+model = Sequential()
+model.add(Conv2D(8,(3,3),activation = ''))
